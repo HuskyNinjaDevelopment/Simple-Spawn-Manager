57d594abe5f8e918e926d9b9b029240abaf0cd38
huskyninja <huskyninjadevelopment@gmail.com>
2024-04-20 18:35:16 -0400

    Rather than execute this script and check every time a player spawns I unregistered the script after it runs the first time.

--- Spawn_Manager.cs
+++ Spawn_Manager.cs
@@ -11,40 +11,35 @@ namespace FivePD_SpawnManager
 {
     internal class Spawn_Manager: Plugin
     {
-        private bool _playerSpawned;
-
-        internal Spawn_Manager() { _playerSpawned = false; }
+        internal Spawn_Manager() { }
 
         [EventHandler("playerSpawned")]
         private async void HandlePlayerSpawn()
         {
-            if (!_playerSpawned) //trigger once when the player spawns in
+            DoScreenFadeOut(0);
+
+            JObject json = JObject.Parse(LoadResourceFile(GetCurrentResourceName(), "plugins/spawn_manager/config.json"));
+            PlayerData playerData = Utilities.GetPlayerData();
+
+            Vector3 newLocation = Vector3.Zero;
+            try
             {
-                DoScreenFadeOut(0);
-
-                JObject json = JObject.Parse(LoadResourceFile(GetCurrentResourceName(), "plugins/spawn_manager/config.json"));
-                PlayerData playerData = Utilities.GetPlayerData();
-
-                Vector3 newLocation = Vector3.Zero;
-                try
-                {
-                    newLocation = JsonConvert.DeserializeObject<Vector3>(json[playerData.DepartmentShortName].ToString());
-                }
-                catch
-                {
-                    newLocation = JsonConvert.DeserializeObject<Vector3>(json[json["DefaultSpawn"].ToString()].ToString());
-                }
-
-                if (newLocation != Vector3.Zero) //Only set this if we have an actual location to send the player
-                {
-                    Game.PlayerPed.Position = newLocation;
-                }
-
-                DoScreenFadeIn(1000);
-                await Delay(1000);
-
-                _playerSpawned = true;
+                newLocation = JsonConvert.DeserializeObject<Vector3>(json[playerData.DepartmentShortName].ToString());
             }
+            catch
+            {
+                newLocation = JsonConvert.DeserializeObject<Vector3>(json[json["DefaultSpawn"].ToString()].ToString());
+            }
+
+            if (newLocation != Vector3.Zero) //Only set this if we have an actual location to send the player
+            {
+                Game.PlayerPed.Position = newLocation;
+            }
+
+            DoScreenFadeIn(1000);
+            await Delay(1000);
+
+            UnregisterScript(this);
         }
     }
 }
