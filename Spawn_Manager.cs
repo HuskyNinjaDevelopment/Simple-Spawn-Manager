daf885904640571e8ce0bb3f9dbd72b1eb434c36
huskyninja <huskyninjadevelopment@gmail.com>
2024-04-20 18:32:16 -0400

    Only force change position when the player spawns in

--- Spawn_Manager.cs
+++ Spawn_Manager.cs
@@ -5,37 +5,46 @@ using System;
 using FivePD.API.Utils;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace FivePD_SpawnManager
 {
     internal class Spawn_Manager: Plugin
     {
-        internal Spawn_Manager() { }
+        private bool _playerSpawned;
+
+        internal Spawn_Manager() { _playerSpawned = false; }
 
         [EventHandler("playerSpawned")]
         private async void HandlePlayerSpawn()
         {
-            DoScreenFadeOut(0);
-
-            JObject json = JObject.Parse(LoadResourceFile(GetCurrentResourceName(), "plugins/spawn_manager/config.json"));
-            PlayerData playerData = Utilities.GetPlayerData();
-
-            Vector3 newLocation = Vector3.Zero;
-            try
+            if (!_playerSpawned) //trigger once when the player spawns in
             {
-                newLocation = JsonConvert.DeserializeObject<Vector3>(json[playerData.DepartmentShortName].ToString());
-            }
-            catch 
-            { 
-                newLocation = JsonConvert.DeserializeObject<Vector3>(json[json["DefaultSpawn"].ToString()].ToString()); }
+                DoScreenFadeOut(0);
 
-            if(newLocation != Vector3.Zero) //Only set this if we have an actual location to send the player
-            {
-                Game.PlayerPed.Position = newLocation;
-            }
+                JObject json = JObject.Parse(LoadResourceFile(GetCurrentResourceName(), "plugins/spawn_manager/config.json"));
+                PlayerData playerData = Utilities.GetPlayerData();
 
-            DoScreenFadeIn(1000);
-            await Delay(1000);
+                Vector3 newLocation = Vector3.Zero;
+                try
+                {
+                    newLocation = JsonConvert.DeserializeObject<Vector3>(json[playerData.DepartmentShortName].ToString());
+                }
+                catch
+                {
+                    newLocation = JsonConvert.DeserializeObject<Vector3>(json[json["DefaultSpawn"].ToString()].ToString());
+                }
+
+                if (newLocation != Vector3.Zero) //Only set this if we have an actual location to send the player
+                {
+                    Game.PlayerPed.Position = newLocation;
+                }
+
+                DoScreenFadeIn(1000);
+                await Delay(1000);
+
+                _playerSpawned = true;
+            }
         }
     }
 }
